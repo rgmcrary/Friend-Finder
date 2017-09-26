09468f9b6e0512bb20635635196d65a31e05eb7a
Rich McRary <rgmcrary@gmail.com>
2017-09-26 18:30:15 -0400

    adjusted code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,23 @@
-// * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-// * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
-
 module.exports = function(app, path) {
+  // Create New User Input and processes it - takes in JSON input
+  app.post('/api/friends', function(req, res) {
+    console.log(req.body);
+    fs.writeFile('../data/friends.js', JSON.stringify(data), function(error) {
+      if (error) {
+        console.log('[write output]: ' + err);
+        if (fail) fail(error);
+      } else {
+        console.log('[write output]: success');
+    //   if (success) success();  
+      }
+    });
+  });
 
-app.get('/api/friends', function(req, res) {
+  // Compare results for a match
+  // fs.readFile(path.join(__dirname, '../data/friends.js')
+
+  //   Displays friends.js arrays of input
+  app.get('/api/friends', function(req, res) {
     res.sendFile(path.join(__dirname, '../data/friends.js'));
   });
-  
-  // Create New User Input - takes in JSON input
-  app.post('/api/friends', function(req, res) {
-    console.log(req.body)
-    var responseText;
-
-  });
-}
+};
