ec1c815bfe262afa4dc6be679b9c8d9d13aa7e80
Rich McRary <rgmcrary@gmail.com>
2017-09-26 11:42:17 -0400

    Created routing connection point

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,10 @@
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'home.html'));
+module.exports = function(app, path) {
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
   });
-  
+
   app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname, 'survey.html'));
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
   });
+};
