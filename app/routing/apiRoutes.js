571ac9dd2c260433c0b9db86861ad4c0b818b7ce
Rich McRary <rgmcrary@gmail.com>
2017-09-26 11:42:08 -0400

    Created routing connection point

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,16 @@
 // * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 // * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
 
+module.exports = function(app, path) {
+
 app.get('/api/friends', function(req, res) {
-    res.sendFile(path.join(__dirname, 'friends.js'));
+    res.sendFile(path.join(__dirname, '../data/friends.js'));
   });
   
   // Create New User Input - takes in JSON input
-  app.post('/reserve', function(req, res) {
+  app.post('/api/friends', function(req, res) {
     console.log(req.body)
     var responseText;
+
+  });
+}
