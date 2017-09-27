22659024f18cc9281638ea09c9c7a764ef12073a
Rich McRary <rgmcrary@gmail.com>
2017-09-27 14:41:13 -0400

    Edit the array set up

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,16 @@
-var user = [
+[
   {
-    name: 'Henry',
-    photo:
-      'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiL5La7isHWAhUDQyYKHS6EBH0QjRwIBw&url=http%3A%2F%2Fwww.mensfitness.com%2Flife%2Fentertainment%2Fhenry-cavill-mens-fitness-feature-justice-league-dating-superman-007&psig=AFQjCNF4zLdhgLHYX2FRX528rYAIBMzA5A&ust=1506454475046448',
-    scores: [1, 3, 1, 1, 1, 2, 1, 5, 1, 5]
+    "name": "Henry",
+    "photo":
+      "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiL5La7isHWAhUDQyYKHS6EBH0QjRwIBw&url=http%3A%2F%2Fwww.mensfitness.com%2Flife%2Fentertainment%2Fhenry-cavill-mens-fitness-feature-justice-league-dating-superman-007&psig=AFQjCNF4zLdhgLHYX2FRX528rYAIBMzA5A&ust=1506454475046448",
+    "scores": [1, 3, 1, 1, 1, 2, 1, 5, 1, 5],
+    "totalScore": 21
   },
   {
-    name: 'Gal',
-    photo:
-      'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwij3IrxisHWAhVG4iYKHXb3DxMQjRwIBw&url=http%3A%2F%2Fawiderbridge.org%2Fgal-gadot-addresses-wonder-womans-bisexuality%2F&psig=AFQjCNHuXS2qjWTwXDFPpLMD__9AU0tnag&ust=1506454533495022',
-    scores: [5, 1, 1, 5, 1, 1, 5, 1, 5, 1]
+    "name": "Gal",
+    "photo":
+      "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwij3IrxisHWAhVG4iYKHXb3DxMQjRwIBw&url=http%3A%2F%2Fawiderbridge.org%2Fgal-gadot-addresses-wonder-womans-bisexuality%2F&psig=AFQjCNHuXS2qjWTwXDFPpLMD__9AU0tnag&ust=1506454533495022",
+    "scores": [5, 1, 1, 5, 1, 1, 5, 1, 5, 1],
+    "totalScore": 26
   }
-];
+]
